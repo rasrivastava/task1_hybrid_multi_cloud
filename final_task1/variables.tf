27fb5ff4de5ab7f574300bcddd8b0e74c0dcd8ad
Rahul Srivastava <rahulsrivastava.bsb@gmail.com>
2020-06-16 02:02:16 +0530

    Add files via upload

---
+++ final_task1/variables.tf
@@ -0,0 +1,31 @@
+variable profile_name {
+    default = "rasrivasprofile"
+}
+
+variable ssh_key_name {
+    default = "EC2KeyPair"
+}
+
+variable firewall_name {
+    default = "MySecurityGroup"
+}
+
+
+variable ami_id {	
+    default = "ami-0447a12f28fddb066"
+}
+
+
+variable instance_name {
+    default = "MyWebServer"
+}
+
+
+variable bucket_name {
+	default = "rasrivas-bucket-1234"
+}
+
+
+variable object_name {
+    default = "image.jpg"
+}
